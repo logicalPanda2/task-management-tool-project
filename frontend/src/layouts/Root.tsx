5aab16ba084b3e79556b5c25d7c663337b5d4d22
Marcel <marcelrg.business@gmail.com>
2026-02-18 23:16:54 +0700

    frontend: Make various UI tweaks for root layout

--- frontend/src/layouts/Root.tsx
+++ frontend/src/layouts/Root.tsx
@@ -3,7 +3,7 @@ import { Link, Outlet } from "react-router-dom";
 export default function Root() {
     return (
         <div className="flex flex-col flex-nowrap min-h-screen relative">
-            <header className="flex flex-col md:flex-row flex-nowrap justify-between md:items-center sticky top-0 p-12 bg-white">
+            <header className="flex flex-col md:flex-row flex-nowrap justify-between md:items-center sticky top-0 px-12 py-6 bg-white z-10 border-b">
                 <h1 className="text-4xl mb-6 md:mb-0">Project Management Tool</h1>
                 <nav className="border-t md:border-0 pt-6 md:pt-0">
                     <ul className="flex flex-row flex-nowrap items-center gap-4 md:gap-8">
@@ -26,7 +26,7 @@ export default function Root() {
                     </ul>
                 </nav>
             </header>
-            <main className="flex flex-col grow flex-nowrap p-12 pt-0">
+            <main className="flex flex-col grow flex-nowrap p-12">
                 <Outlet />
             </main>
         </div>
