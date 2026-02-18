c83b748ff2435d9b63659b0125b06de66e2c9d56
Marcel <marcelrg.business@gmail.com>
2026-02-18 19:42:05 +0700

    frontend: Add 404 not found page

--- frontend/src/pages/NotFound.tsx
+++ frontend/src/pages/NotFound.tsx
@@ -1,5 +1,11 @@
+import { Link } from "react-router-dom";
+
 export default function NotFound() {
     return (
-        <p>Not found placeholder</p>
+        <main className="min-h-screen p-12">
+            <h1 className="text-4xl mb-8">404 Not Found</h1>
+            <p className="text-2xl mb-4">The page you were looking for does not exist.</p>
+            <Link to={"/"} className="text-2xl underline hover:no-underline focus-visible:outline-0 focus-visible:no-underline">Back to Home</Link>
+        </main>
     );
 }
