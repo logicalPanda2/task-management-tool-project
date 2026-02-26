8ae989ef5dc9334629e5e3411eff8c56828b77c3
Marcel <marcelrg.business@gmail.com>
2026-02-26 18:39:59 +0700

    backend: Change log middleware to use response status and message

--- backend/src/shared/logMiddleware.ts
+++ backend/src/shared/logMiddleware.ts
@@ -6,7 +6,7 @@ export default function logMiddleware(req: Request, res: Response, next: (...arg
     res.on("finish", () => {
         const durationMs = Date.now() - startTimeInMs;
         console.log(
-            `${req.method} ${req.originalUrl}\nDone in ${durationMs}ms\nFinished with status ${req.statusCode}\nwith the following message ${req.statusMessage}\n`
+            `${req.method} ${req.originalUrl}\nDone in ${durationMs}ms\nFinished with status ${res.statusCode}\nwith the following message ${res.statusMessage}\n`
         );
     });
 
