95d808a43827bef56da45b72a75d3c74ca94dba2
Marcel <marcelrg.business@gmail.com>
2026-02-26 17:47:29 +0700

    backend: Add error middleware to app.ts

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -1,11 +1,17 @@
 import cookieParser from "cookie-parser";
 import express from "express";
 import logMiddleware from "./shared/logMiddleware.js";
+import errorMiddleware from "./shared/errorMiddleware.js";
 
 const app = express();
 
 app.use(express.json());
 app.use(cookieParser());
 app.use(logMiddleware);
+// auth middleware
+
+// routes
+
+app.use(errorMiddleware);
 
 export default app;
