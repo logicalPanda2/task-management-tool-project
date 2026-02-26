63ea52408a564aced2fba35305e47dda54dc482f
Marcel <marcelrg.business@gmail.com>
2026-02-26 17:16:43 +0700

    backend: Add log middleware to app.ts

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -1,9 +1,11 @@
 import cookieParser from "cookie-parser";
 import express from "express";
+import logMiddleware from "./shared/logMiddleware.js";
 
 const app = express();
 
 app.use(express.json());
 app.use(cookieParser());
+app.use(logMiddleware);
 
 export default app;
