39d5041d9faf739393c8d99af78e9df86f2863f6
Marcel <marcelrg.business@gmail.com>
2026-02-27 17:30:56 +0700

    backend: Add generate refresh token function

--- backend/src/features/auth/authService.ts
+++ backend/src/features/auth/authService.ts
@@ -6,7 +6,6 @@ dotenv.config();
 export function generateAccessToken(user: User) {
     if(
         !process.env.JWT_ACCESS_SECRET ||
-        !process.env.JWT_REFRESH_SECRET ||
         !process.env.ACCESS_TOKEN_DURATION
     ) throw new Error("Secret keys not configured. Check .env.example for more information.");
 
@@ -22,6 +21,21 @@ export function generateAccessToken(user: User) {
     // assert as any to bypass StringValue type limitations on parameter
     // as reverse engineering the type definition isn't worth it.
     // make sure that the env variable complies with the "[number][m | d]" format
-    
+
     // will upgrade to safer and more robust version in due time.
+}
+
+export function generateRefreshToken(user: User) {
+    if(
+        !process.env.JWT_REFRESH_SECRET ||
+        !process.env.REFRESH_TOKEN_DURATION
+    ) throw new Error("Secret keys not configured. Check .env.example for more information.");
+
+    return jwt.sign(
+        { id: user.id },
+        process.env.JWT_REFRESH_SECRET,
+        { expiresIn: process.env.REFRESH_TOKEN_DURATION as any },
+    );
+
+    // same reason for as any assertion as explained above
 }
