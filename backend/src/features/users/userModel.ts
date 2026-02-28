701cf4b2158da53f506badd46317564aa7a8494e
Marcel <marcelrg.business@gmail.com>
2026-02-28 10:25:00 +0700

    backend: Adjust user model return values to account for nonexistence

--- backend/src/features/users/userModel.ts
+++ backend/src/features/users/userModel.ts
@@ -1,6 +1,6 @@
 import pool from "../../config/db.js";
 
-export async function getUserByEmail(email: string): Promise<User> {
+export async function getUserByEmail(email: string): Promise<User | undefined> {
     const result = await pool?.query(
         `SELECT id, email, password FROM users WHERE email = $1;`,
         [email]
