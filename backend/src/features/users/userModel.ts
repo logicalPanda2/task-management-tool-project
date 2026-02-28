29221ee37e4f14c9225ef71c4fb98c0de5ae8256
Marcel <marcelrg.business@gmail.com>
2026-02-28 10:40:54 +0700

    backend: Add new query to get by id

--- backend/src/features/users/userModel.ts
+++ backend/src/features/users/userModel.ts
@@ -8,3 +8,12 @@ export async function getUserByEmail(email: string): Promise<User | undefined> {
 
     return result?.rows[0];
 }
+
+export async function getUserById(id: string): Promise<User | undefined> {
+    const result = await pool?.query(
+        `SELECT id, email, password FROM users WHERE id = $1;`,
+        [id]
+    );
+
+    return result?.rows[0];
+}
