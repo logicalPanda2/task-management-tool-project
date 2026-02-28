ad2bdde9a7008c01960f36c349b83baefe4cd4e3
Marcel <marcelrg.business@gmail.com>
2026-02-28 16:21:58 +0700

    backend: Add getAllProjects

--- backend/src/features/projects/projectController.ts
+++ backend/src/features/projects/projectController.ts
@@ -1,5 +1,19 @@
-export function getAllProjects() {
+import type { Request, Response } from "express";
+import { getAllProjectMetadataByUserId } from "./projectModel.js";
 
+export async function getAllProjects(req: Request, res: Response, next: (...args: any[]) => any) {
+    try {
+        const user = req.user;
+        if(!user) return res.sendStatus(401);
+
+        const projects: ProjectMetadata[] = await getAllProjectMetadataByUserId(user.id);
+
+        res.json({ projects: projects });
+    } catch(e) {
+        next(e);
+    }
+
+    return undefined;
 }
 
 export function getProject() {
