1ded546ad132294699b7a128caa060f5eb7e6e15
Marcel <marcelrg.business@gmail.com>
2026-02-28 11:46:45 +0700

    backend: Mount comment and task routes to project router

--- backend/src/features/projects/projectRouter.ts
+++ backend/src/features/projects/projectRouter.ts
@@ -1,4 +1,6 @@
 import express from "express";
+import taskRouter from "../tasks/taskRouter.js";
+import commentRouter from "../comments/commentRouter.js";
 
 const projectRouter = express.Router();
 
@@ -6,5 +8,7 @@ projectRouter.get("/");
 projectRouter.get("/:projectId");
 projectRouter.post("/:projectId");
 projectRouter.delete("/:projectId");
+projectRouter.use("/:projectId/tasks", taskRouter);
+projectRouter.use("/:projectId/comments", commentRouter);
 
 export default projectRouter;
