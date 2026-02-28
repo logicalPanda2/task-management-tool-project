ab6cefffd4c9eaf389b61f346ece88c32f8384e7
Marcel <marcelrg.business@gmail.com>
2026-02-28 10:25:23 +0700

    backend: Add login function

--- backend/src/features/auth/authController.ts
+++ backend/src/features/auth/authController.ts
@@ -1,9 +1,29 @@
 import type { Request, Response } from "express";
+import { generateAccessToken, generateRefreshToken, verifyUserCredentials } from "./authService.js";
 
-export function login(req: Request, res: Response, next: (...args: any[]) => any) {
-    res.json({
-        message: "Credentials got"
-    });
+export async function login(req: Request, res: Response, next: (...args: any[]) => any) {
+    try {
+        const { email, password } = req.body;
+
+        const user = await verifyUserCredentials(email, password);
+
+        if(!user) return res.status(401);
+
+        const accessToken = generateAccessToken(user);
+        const refreshToken = generateRefreshToken(user);
+
+        res.cookie( "refreshToken", refreshToken, {
+            httpOnly: true,
+            secure: true,
+            sameSite: "strict",
+        });
+
+        res.json({ accessToken });
+    } catch(e) {
+        next(e);
+    }
+    
+    return undefined;
 }
 
 export function refresh(req: Request, res: Response, next: (...args: any[]) => any) {
