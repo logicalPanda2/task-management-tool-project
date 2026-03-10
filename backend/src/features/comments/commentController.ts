a940f5a15ea4c91d70affe72692d703ee2f66613
Marcel <marcelrg.business@gmail.com>
2026-03-10 19:25:41 +0700

    Add getall and remove comment controllers

--- backend/src/features/comments/commentController.ts
+++ backend/src/features/comments/commentController.ts
@@ -1,11 +1,59 @@
-export function getAll() {
+import type { Request, Response } from "express";
+import * as commentRepo from "./commentRepo.js";
+import * as Services from "./commentService.js";
 
+export async function getAll(req: Request, res: Response, next: (...args: any[]) => any) {
+    try {
+        if(
+            !("projectId" in req.params) ||
+            typeof req.params.projectId !== "string"
+        ) return res.sendStatus(400);
+
+        const projectId = req.params.projectId;
+        const comments = await commentRepo.getAllByProjectId(projectId);
+
+        return res.json({
+            comments: comments
+        });
+    } catch(e) {
+        next(e);
+    }
+
+    return undefined;
 }
 
-export function create() {
+export async function create(req: Request, res: Response, next: (...args: any[]) => any) {
+    try {
+        if(
+            !("projectId" in req.params) ||
+            typeof req.params.projectId !== "string"
+        ) return res.sendStatus(400);
 
+        // service call here
+
+        return res.sendStatus(204);
+    } catch(e) {
+        next(e);
+    }
+
+    return undefined;
 }
 
-export function remove() {
+export async function remove(req: Request, res: Response, next: (...args: any[]) => any) {
+    try {
+        if(
+            !("commentId" in req.params) ||
+            typeof req.params.commentId !== "string"
+        ) return res.sendStatus(400);
 
+        const id = req.params.commentId;
+
+        await commentRepo.deleteById(id);
+
+        return res.sendStatus(204);
+    } catch(e) {
+        next(e);
+    }
+
+    return undefined;
 }
