94890cd3177e643dd43fedda20172d45823ccc51
Marcel <marcelrg.business@gmail.com>
2026-03-10 19:04:09 +0700

    Add getAll for task controller

--- backend/src/features/tasks/taskController.ts
+++ backend/src/features/tasks/taskController.ts
@@ -1,11 +1,30 @@
-export function getAll() {
+import type { Request, Response } from "express";
+import * as taskRepo from "./taskRepo.js";
 
+export async function getAll(req: Request, res: Response, next: (...args: any[]) => any) {
+    try {
+        if(
+            !("projectId" in req.params) ||
+            typeof req.params.projectId !== "string"
+        ) return res.sendStatus(400);
+
+        const projectId = req.params.projectId;
+        const tasks = await taskRepo.getAllByProjectId(projectId);
+
+        res.json({
+            tasks: tasks
+        });
+    } catch(e) {
+        next(e);
+    }
+
+    return undefined;
 }
 
-export function create() {
-
+export async function create(req: Request, res: Response, next: (...args: any[]) => any) {
+    
 }
 
-export function remove() {
+export async function remove(req: Request, res: Response, next: (...args: any[]) => any) {
 
 }
