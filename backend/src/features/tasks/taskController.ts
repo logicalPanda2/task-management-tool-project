df7105aca96c11ca9b25f52130c03241365e0f76
Marcel <marcelrg.business@gmail.com>
2026-03-10 19:12:00 +0700

    Add create task function

--- backend/src/features/tasks/taskController.ts
+++ backend/src/features/tasks/taskController.ts
@@ -1,5 +1,6 @@
 import type { Request, Response } from "express";
 import * as taskRepo from "./taskRepo.js";
+import { isTask, isTaskArray } from "../../shared/utils/typeGuards.js";
 
 export async function getAll(req: Request, res: Response, next: (...args: any[]) => any) {
     try {
@@ -22,7 +23,29 @@ export async function getAll(req: Request, res: Response, next: (...args: any[])
 }
 
 export async function create(req: Request, res: Response, next: (...args: any[]) => any) {
-    
+    try {
+        if(
+            !("projectId" in req.params) ||
+            typeof req.params.projectId !== "string"
+        ) return res.sendStatus(400);
+
+        const projectId = req.params.projectId;
+        const tasks = req.body;
+
+        if(isTaskArray(tasks)) {
+            const task = tasks[0];
+            
+            if(isTask(task)) await taskRepo.createMany(tasks, projectId);
+        } else if(isTask(tasks)) {
+            await taskRepo.create(tasks, projectId);
+        } else res.sendStatus(400);
+
+        res.sendStatus(204);
+    } catch(e) {
+        next(e);
+    }
+
+    return undefined;
 }
 
 export async function remove(req: Request, res: Response, next: (...args: any[]) => any) {
