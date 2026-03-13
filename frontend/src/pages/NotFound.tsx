39b1d3e1943f84caaa87666a3ca084d5374966a7
Marcel <marcelrg.business@gmail.com>
2026-03-13 13:12:13 +0700

    style: rework 404 not found page

--- frontend/src/pages/NotFound.tsx
+++ frontend/src/pages/NotFound.tsx
@@ -2,17 +2,22 @@ import { Link } from "react-router-dom";
 
 export default function NotFound() {
 	return (
-		<main className="min-h-screen p-12">
-			<h1 className="text-4xl mb-4">404 Not Found</h1>
-			<p className="text-2xl mb-8">
-				The page you were looking for does not exist.
-			</p>
-			<Link
-				to={"/"}
-				className="text-2xl underline hover:no-underline focus-visible:outline-0 focus-visible:no-underline"
-			>
-				Back to Home
-			</Link>
+		<main className="min-h-screen flex justify-center items-center">
+			<h1 className="flex flex-col flex-nowrap items-center text-center">
+                <span className="text-8xl text-primary mb-6">404</span>
+                <span className="text-2xl text-secondary mx-12 mb-8">The page you were looking for does not exist.</span>
+                <div className="hover:transform-[translateY(-1px)] transition-custom-all w-fit">
+                    <Link
+                        to={"/"}
+                        className="bg-gradient shadow-default text-primary px-4 py-1.5 rounded-lg active:shadow-pressed active:bg-gradient-pressed active:text-secondary focus-visible:outline-1 transition-custom-all hover:text-accent flex flex-row flex-nowrap items-center stroke-neutral-800 hover:stroke-accent"
+                    >
+                        Go back
+                        <svg className="fill-none stroke-inherit stroke-[1.5px] inline-block w-4 ml-1 mt-0.5" viewBox="0 0 24 24">
+                            <path d="M5 12h14M13 6l6 6-6 6"/>
+                        </svg>
+                    </Link>
+                </div>
+            </h1>
 		</main>
 	);
 }
