8113e3c92c8f3ace3b158741fa181d104111c388
Marcel <marcelrg.business@gmail.com>
2026-03-14 10:47:32 +0700

    feat: add interceptors to axios instance

--- frontend/src/api/api.ts
+++ frontend/src/api/api.ts
@@ -9,4 +9,24 @@ const api = axios.create({
     }
 });
 
+api.interceptors.request.use(
+    (config) => {
+        const token = localStorage.getItem("token");
+        if(token) config.headers.Authorization = `Bearer ${token}`;
+        return config;
+    },
+    (err) => Promise.reject(err),
+);
+
+api.interceptors.response.use(
+    (res) => res,
+    (err) => {
+        if(err?.response?.status === 401) {
+            localStorage.removeItem("token");
+            window.location.href = "/login";
+        }
+        return Promise.reject(err);
+    }
+);
+
 export default api;
