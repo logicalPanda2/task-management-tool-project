f1ac46cf927f332ededf902740275c3afebcee23
Marcel <marcelrg.business@gmail.com>
2026-03-14 12:03:09 +0700

    feat: protect routes

--- frontend/src/routes/AppRoutes.tsx
+++ frontend/src/routes/AppRoutes.tsx
@@ -5,15 +5,16 @@ import ProjectView from "../pages/ProjectView";
 import ProjectEdit from "../pages/ProjectEdit";
 import Home from "../pages/Home";
 import NotFound from "../pages/NotFound";
+import ProtectedRoute from "../pages/ProtectedRoute";
 
 export default function AppRoutes() {
 	return (
 		<Routes>
 			<Route path="/" element={<Root />}>
-				<Route index element={<Home />} />
-				<Route path="project/:id" element={<ProjectView />} />
-                <Route path="project/:id/edit" element={<ProjectEdit />} />
-                <Route path="project/new" element={<ProjectEdit />} />
+				<Route index element={<ProtectedRoute children={<Home />} />} />
+				<Route path="project/:id" element={<ProtectedRoute children={<ProjectView />} />} />
+                <Route path="project/:id/edit" element={<ProtectedRoute children={<ProjectEdit />} />} />
+                <Route path="project/new" element={<ProtectedRoute children={<ProjectEdit />} />} />
 			</Route>
             <Route path="/register" element={<Login />} />
 			<Route path="/login" element={<Login />} />
